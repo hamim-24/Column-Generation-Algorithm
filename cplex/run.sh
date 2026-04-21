4e59fa6bfba93dbd78112ed25bad1e86d921ee13
hamimLohani <hamimlohani@gmail.com>
2026-04-21 22:33:28 +0600

    added main menu

--- cplex/run.sh
+++ cplex/run.sh
@@ -3,6 +3,7 @@
 CPLEX_JAR="/Users/Inz_mac/Applications/CPLEX_Studio_Community2212/cplex/lib/cplex.jar"
 CPLEX_LIB_PATH="/Users/Inz_mac/Applications/CPLEX_Studio_Community2212/cplex/bin/arm64_osx"
 
+# it will find the run.sh file path and cd to it
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR" || exit 1
 
@@ -19,14 +20,8 @@ fi
 
 mkdir -p bin bin/csp bin/vrp
 
-echo "Compiling root Java menu launcher..."
-javac -d bin -cp ".:$CPLEX_JAR" src/Main.java
-if [ $? -ne 0 ]; then
-    echo "Compilation failed."
-    exit 1
-fi
-
 echo "Compiling CSP sources..."
+# find all .java file and sort and pass these as arg to javac
 find CSP/src -name "*.java" | sort | xargs javac -d bin/csp -cp ".:$CPLEX_JAR"
 if [ $? -ne 0 ]; then
     echo "CSP compilation failed."
@@ -40,5 +35,48 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Running project menu..."
-java --enable-native-access=ALL-UNNAMED -cp "bin:$CPLEX_JAR" -Djava.library.path="$CPLEX_LIB_PATH" Main "$CPLEX_JAR" "$CPLEX_LIB_PATH"
+# ./run.sh csp
+if [ "$1" == "csp" ]; then
+    echo "Launching CSP..."
+    cd CSP || exit 1
+    java --enable-native-access=ALL-UNNAMED -cp "../bin/csp:$CPLEX_JAR" -Djava.library.path="$CPLEX_LIB_PATH" CSPMain
+
+# ./run.sh vrp
+elif [ "$1" == "vrp" ]; then
+    echo "Launching VRP..."
+    cd VRP || exit 1
+    java --enable-native-access=ALL-UNNAMED -cp "../bin/vrp:$CPLEX_JAR" -Djava.library.path="$CPLEX_LIB_PATH" VRPMain
+
+# ./run.sh
+else
+    while true; do
+        echo ""
+        echo "=== Column Generation Project Menu ==="
+        echo "1) CSP"
+        echo "2) VRP"
+        echo "3) Exit"
+        echo -n "Enter choice: "
+        read choice
+        case $choice in
+            1)
+                echo "Launching CSP..."
+                cd CSP || exit 1
+                java --enable-native-access=ALL-UNNAMED -cp "../bin/csp:$CPLEX_JAR" -Djava.library.path="$CPLEX_LIB_PATH" CSPMain
+                # exit 0
+                ;;
+            2)
+                echo "Launching VRP..."
+                cd VRP || exit 1
+                java --enable-native-access=ALL-UNNAMED -cp "../bin/vrp:$CPLEX_JAR" -Djava.library.path="$CPLEX_LIB_PATH" VRPMain
+                # exit 0
+                ;;
+            3)
+                echo "Exiting..."
+                exit 0
+                ;;
+            *)
+                echo "Invalid choice. Please enter 1, 2 or 3."
+                ;;
+        esac
+    done
+fi
